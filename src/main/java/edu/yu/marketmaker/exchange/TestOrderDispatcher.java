03f5922aa22574abe8cff67495452128e3a36c61
Yisroel Newmark <ymnewmark@gmail.com>
2026-02-11 13:34:54 -0500

    Test Order Dispatcher logs orders

--- src/main/java/edu/yu/marketmaker/exchange/TestOrderDispatcher.java
+++ src/main/java/edu/yu/marketmaker/exchange/TestOrderDispatcher.java
@@ -1,5 +1,7 @@
 package edu.yu.marketmaker.exchange;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import edu.yu.marketmaker.model.ExternalOrder;
@@ -7,10 +9,12 @@ import edu.yu.marketmaker.model.ExternalOrder;
 @Component
 public class TestOrderDispatcher implements OrderDispatcher {
 
+    private static final Logger logger = LoggerFactory.getLogger(TestOrderDispatcher.class);
+
     @Override
     public void dispatchOrder(ExternalOrder order) {
-        // TODO: add logging
-        System.out.println(order);
+        logger.info("Dispatching {} order: {} x {} @ {}",
+            order.side(), order.symbol(), order.quantity(), order.limitPrice());
     }
     
 }
