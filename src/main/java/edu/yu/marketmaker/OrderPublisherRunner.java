9fb272ca9aa5d6479e648d3c19d16bf733eb5d3e
Matias Salem <matiassalem17@gmail.com>
2026-02-24 17:38:20 -0500

    Reapply "configure EOP to work with exchange"
    This reverts commit ea909492488357b27c5a29eb876d0ac036ce78bf.

--- src/main/java/edu/yu/marketmaker/OrderPublisherRunner.java
+++ src/main/java/edu/yu/marketmaker/OrderPublisherRunner.java
@@ -1,15 +1,22 @@
 package edu.yu.marketmaker;
 
 import edu.yu.marketmaker.external.ExternalOrderPublisher;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
 
-public class OrderPublisherRunner {
-    public static void main(String[] args) {
-        String exchangeBaseUrl = "http://localhost:8080/orders"; // Adjust as needed
-        try {
-            ExternalOrderPublisher publisher = new ExternalOrderPublisher(exchangeBaseUrl);
-            publisher.startGeneratingOrders();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+@Component
+@Profile("external-publisher")
+public class OrderPublisherRunner implements ApplicationRunner {
+
+    @Value("${exchange.base-url}")
+    private String exchangeBaseUrl;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        ExternalOrderPublisher publisher = new ExternalOrderPublisher(exchangeBaseUrl);
+        publisher.startGeneratingOrders();
     }
 }
